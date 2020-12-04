c4e4d82ab3e24f89cf225e76a80e62d0e52cf10e
guxal <jonathanacp93@gmail.com>
2020-12-04 17:12:49 +0000

    add description fullhidden

--- 0x00-ls/hls.h
+++ 0x00-ls/hls.h
@@ -5,11 +5,13 @@
 #include <dirent.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * struct Option - Options send for user
  * @vertical: is vertical options
  * @hidden: show hidden files
+ * @fullhidden: do not list implied . and ..
  */
 typedef struct Option
 {
