176986da37a62a82fb05512508bb382e0848ea95
guxal <jonathanacp93@gmail.com>
2020-12-14 00:20:38 +0000

    update code laps

--- 0x01-getline/laps.c
+++ 0x01-getline/laps.c
@@ -1,105 +1,26 @@
 #include "laps.h"
-/*
- * Appends a single element at the head of the list
- * List is passed by address, so the content of the caller
- * variable is updated to point to the new head
- *
- * Complexity: O( 1 )
-*/
-void list_add_node( Nascar **list, Nascar *node )
-{
-    node->next = *list;
-    *list = node;
-}
- 
-/*
- * Append list at the end of another list
- * Head list is passed by address, so the content of the caller
- * variable can be updated to poin to the new head
- * if the original list was empty (NULL)
- *
- * Complexity: O( len(l0) )
- */
-void list_concat( Nascar **l0, Nascar *l1 )
-{
-    /* We find the address of the pointer that needs to be updated */
-    while (*l0)
-        l0 = &((*l0)->next);
-   
-    /* We update it to point to the new tail */
-    *l0 = l1;
-}
- 
-/*
- * Quick sort of the list
- * This is a naive quicksort implementation:
- * a) It will decay into n^2 complexity if the list is already sorted
- * b) It scans the list multiple time to concatenate
- *    (this can easily be avoided by keeping track of the address of the end of left)
- *
- * Complexity:
- *   best     : O( len(list) )
- *   average  : O( len(list)*log(len(list)) )
- *   worst    : O( len(list)^2 )
- */
-void my_qsort( Nascar **list )
-{
-    /* Sorting an empty list is trivial */
-    if (!*list)
-        return;
- 
-    /* Extract the pivot */    
-    Nascar *pivot = *list;
-    int data = pivot->id;
-    Nascar *p = pivot->next;
-    pivot->next = NULL;
- 
-    /* Construct left and right lists in place in a single pass*/
-    Nascar *left = NULL;
-    Nascar *right = NULL;
- 
-    while (p)
-    {
-        Nascar *n = p;  
-        p = p->next;
-        if (n->id<data)
-            list_add_node( &left, n );
-        else
-            list_add_node( &right, n );
-    }
- 
-    /* We now sort left and right */
-    /* If left and right are of vastly different lengths, the complexity won't be O(n log n) */
-    my_qsort( &left );
-    my_qsort( &right );
- 
-    /* We now concatenate lists [this is inefficient, but doesn't hurt complexity] */
-    Nascar *result = NULL;
-    list_concat( &result, left );
-    list_concat( &result, pivot );
-    list_concat( &result, right );
-    *list = result;
-}
 
 /**
- * _strlen - strlen
- * @str: string
- * Return: string
+ * insertsort - inserts linked list in a sorted order
+ * @head: pointer to pointer of head node
+ * @new: pointer to new node
  */
-
-int _strlen(const char *str)
+void insertsort(Nascar **head, Nascar *new)
 {
-	int count = 0;
+	Nascar **cur;
 
-	while (str[count])
-		++count;
-	return (count);
+	cur = head;
+	while (*cur && new->id > (*cur)->id)
+		cur = &(*cur)->next;
+	new->next = *cur;
+	*cur = new;
 }
+
 /**
  * *add_node - add node in list
  * @head: head
- * @str: string
- * Return: List_t
+ * @id: id new car
+ * Return: Nascar struct
  */
 Nascar *add_node(Nascar **head, const int id)
 {
@@ -110,125 +31,84 @@ Nascar *add_node(Nascar **head, const int id)
 	new = malloc(sizeof(Nascar));
 	if (new == NULL)
 		return (NULL);
-    
+
 	new->id = id;
 	new->laps = 0;
-	new->next = *head;
-	*head = new;
-
+	/* new->next = *head; *head = new; */
+	insertsort(head, new);
 	return (new);
 }
 
-void print_race_state(Nascar *idn)
+/**
+ * print_race_state - print race state
+ * @head: head node
+ */
+void print_race_state(Nascar *head)
 {
-    printf("Race state:\n");
-    Nascar *tmp = idn;
+	printf("Race state:\n");
+	Nascar *tmp = head;
 
-    while(tmp != NULL)
-    {
-        printf("Car %d [%zu laps]\n", tmp->id, tmp->laps);
-        tmp = tmp->next;
-    }
+	while (tmp != NULL)
+	{
+		printf("Car %d [%zu laps]\n", tmp->id, tmp->laps);
+		tmp = tmp->next;
+	}
 }
 
+/**
+ * print_newcar - print new car string
+ * @head: head node
+ * @i: index new card
+ */
+void print_newcar(Nascar **head, int i)
+{
+	add_node(head, i);
+	printf("Car %d Joined the race\n", i);
+}
+
+/**
+ * race_state - race state
+ * @id: list of lists the id uniq
+ * @size: size lists
+ */
 void race_state(int *id, size_t size)
 {
-    //static int **ids = NULL;
-    static Nascar *idn = NULL;
-    //(void) *ids;
-    //(void) size;
-    Nascar *free_tmp;
-    if (!size)
+	static Nascar *head;
+	Nascar *free_tmp;
+
+
+	if (!size)
 	{
-		while (idn)
+		while (head)
 		{
-			free_tmp = idn;
-			idn = idn->next;
+			free_tmp = head, head = head->next;
 			free(free_tmp);
-		}
-		return;
+		} return;
 	}
+	if (head == NULL)
+	{
+		for (size_t i = 0; i < size; i++)
+			print_newcar(&head, id[i]);
+		print_race_state(head);
+	} else
+	{
+		Nascar *tmp = head;
+		int exist;
 
-    if (idn == NULL){
-        //ids = (int**) malloc(sizeof(int*) * size + 1);
-        //memset(ids, 0x0, sizeof(int*) * size + 1);
-        //idn = malloc(sizeof(Nascar));
-        //if (ids == NULL)
-		//    return;
-        //printf("%x", ids);
-        //printf("%d", *ids);
-        //printf("%d", **ids);
-
-        for (size_t i = 0; i < size; i++)
-        {
-
-            //printf("%i\n", id[i]);
-            add_node(&idn, id[i]);
-            //ids[i] = (int*) malloc(sizeof(int) * 2);
-            //memset(ids[i], 0x00, sizeof(int) * 2);
-        //    /* code */
-            //ids[i][0] = id[i];
-            //ids[i][1] = 0;
-////
-            printf("Car %d Joined the race\n", id[i]);
-        }
-
-        my_qsort(&idn);
-        //ids[i] = NULL;
-        // printf("%d\n", ids[0][0]);
-        // printf("%d\n", ids[1][0]);
-        // printf("%d\n", ids[2][0]);
-        print_race_state(idn);
-    } else {
-
-        //int count = 0;
-        Nascar *tmp = idn;
-        Nascar *tmp2 = idn;
-        while(tmp != NULL)
-        {
-            for (size_t i = 0; i < size; i++)
-            {
-                if (tmp->id == id[i])
-                {
-                    tmp->laps++;
-                }
-            }
-        //    count++;
-            tmp = tmp->next;
-        }
-
-        int exist;
-        //int count2;
-        for(size_t i = 0; i < size; i++)
-        {
-            exist = 0;
-        //    count2 = 0;
-            while(tmp2 != NULL)
-            {
-                if (tmp2->id == id[i])
-                {
-                    exist = 1;
-                }
-                tmp2 = tmp2->next;
-            }
-
-            tmp2 = idn;
-
-            if (exist == 0)
-            {
-                //printf("dont exists\n");
-                add_node(&idn, id[i]);
-                printf("Car %d Joined the race\n", id[i]);
-            }
-        }
-        my_qsort(&idn);
-        print_race_state(idn);
-
-    }
-    /* while(*id){
-       printf("%i\n", *id);
-       id++;
-    } */
+		for (size_t i = 0; i < size; i++)
+		{
+			exist = 0;
+			while (tmp != NULL)
+			{
+				if (tmp->id == id[i])
+					exist = 1, tmp->laps++;
+				tmp = tmp->next;
+			} tmp = head;
+			if (exist == 0)
+				print_newcar(&head, id[i]);
+		}
+		print_race_state(head);
+	}
 }
 
 
