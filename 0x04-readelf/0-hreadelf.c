7baf35bc290ab5dc8aba21b85c988cc560385658
guxal <jonathanacp93@gmail.com>
2021-01-31 20:38:54 +0000

    separe functions and add printelf_0

--- 0x04-readelf/0-hreadelf.c
+++ 0x04-readelf/0-hreadelf.c
@@ -143,73 +143,8 @@ void printosabi(char *ident)
 }
 
 
-/* ---------------- */
 
 
-/***/
-int printversion(char *ident, char *str)
-{
-	if (ident[EI_VERSION] == EV_NONE)
-	{
-		fprintf(stderr, "Error");
-		return (1);
-	}
-	printf("%9s%-28c%i (current)\n", "Version", ':', EV_CURRENT);
-	return (0);
-}
-
-/***/
-int printdata(char *ident, char *str)
-{
-	if (ident[EI_DATA] == ELFDATANONE)
-	{
-		fprintf(stderr, "Error");
-		return (1);
-	}
-	if (ident[EI_DATA] == ELFDATA2LSB)
-	{
-		printf("%6s%-31c%s%s\n", "Data", ':', "2's complement, ",
-				"little endian");
-	}
-	else
-	{
-		printf("%6s%-31c%s%s\n", "Data", ':', "2's complement, ",
-				"big endian");
-	}
-	return (0);
-}
-
-/***/
-int printclass(char *ident, char *str)
-{
-	if (ident[EI_CLASS] == ELFCLASSNONE)
-	{
-		fprintf(stderr, "Error");
-		return (1);
-	}
-	if (ident[EI_CLASS] == ELFCLASS32)
-	{
-		printf("%7s%-30c%s\n", "Class", ':', "ELF32");
-	}
-	else
-	{
-		printf("%7s%-30c%s\n", "Class", ':', "ELF64");
-	}
-	return (0);
-}
-
-/***/
-void _printmag(char *ident)
-{
-	size_t i;
-
-	printf("%11s", "Magic:   ");
-	for (i = 0; i < EI_NIDENT; ++i)
-		printf("%02x ", ident[i]);
-	putchar('\n');
-}
-
-/* ------------------------------- */
 
 FILE *get_fd(char *name)
 {
--- 0x04-readelf/hreadelf.h
+++ 0x04-readelf/hreadelf.h
@@ -9,10 +9,23 @@
 #include <unistd.h>
 #include <elf.h>
 
+/**
+ * struct instruction_s - code and str
+ * @code: the byte code
+ * @str: string to print
+ *
+ * Description: code and str
+ * for hreadelf HOLBERTON
+ */
 typedef struct instruction_s
 {
 	int code;
 	char *str;
 } instruction_t;
 
+
+int printversion(char *ident, char *str);
+int printdata(char *ident, char *str);
+int printclass(char *ident, char *str);
+void _printmag(char *ident);
 #endif
