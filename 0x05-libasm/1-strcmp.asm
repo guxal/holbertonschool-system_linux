dd55b26039e3401075dc9b6e2650f57aca78bf24
guxal <jonathancp93@gmail.com>
2021-02-10 19:36:33 +0000

    add const

--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -5,29 +5,27 @@ BITS 64
 
 asm_strcmp:
         movsx   edx, byte [rdi]
-        test    dl, dl
-        jz      ?_003 
+        test    dl, dl         
+        jz      ?_003          
         movsx   ecx, byte [rsi]
         xor     eax, eax       
-        cmp     dl, cl
-        jz      ?_002 
-        jmp     ?_004 
+        cmp     dl, cl         
+        jz      ?_002          
+        jmp     ?_004          
 
-?_001:  add     rax, 1
-        movsx   ecx, byte [rsi+rax]     
-        cmp     dl, cl
-        jnz     ?_004 
-?_002:  movsx   edx, byte [rdi+rax+1H]  
-        test    dl, dl
-        jnz     ?_001 
+?_001:  add     rax, 1         
+        movsx   ecx, byte [rsi+rax]                     
+        cmp     dl, cl         
+        jnz     ?_004          
+?_002:  movsx   edx, byte [rdi+rax+1H]                  
+        test    dl, dl         
+        jnz     ?_001          
 ?_003:  xor     eax, eax       
-        ret  
+        ret                    
 
 ?_004:  sub     edx, ecx       
         xor     eax, eax       
         test    edx, edx       
-        setg    al    
-        lea     eax, [rax+rax-1H]       
-        ret  
-                                       
-
+        setg    al             
+        lea     eax, [rax+rax-1H]                       
+        ret
