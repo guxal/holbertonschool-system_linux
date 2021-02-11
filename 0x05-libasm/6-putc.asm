b4cca7f719ef0ce97f25de8ae1f854519473630f
guxal <jonathancp93@gmail.com>
2021-02-11 13:48:09 +0000

    change logic code

--- 0x05-libasm/6-putc.asm
+++ 0x05-libasm/6-putc.asm
@@ -1,19 +1,31 @@
 BITS 64
-	global asm_putc	; export 'asm_putc' function
 
-	section .text
+    global asm_putc
 
-asm_putc:  ; Function begin
-        sub     rsp, 24             
-        mov     edx, 1              
-        lea     rsi, [rsp+0CH]      
-        mov     dword [rsp+0CH], edi
-        mov     edi, 1              
-        mov rax, 1                  ; write syscall
-        mov rsi, rsp                ; Character
-        mov rdi, 1                  ; stdout
-        mov rdx, 1                  ; one byte
-        syscall           
-        add     rsp, 24             
-        ret                         
-; asm_putc End of function
+    section .text
+
+asm_putc:
+
+    push rbp                        ; prologue
+    mov rbp, rsp
+
+    push rsi
+    push rdx
+    push rdi
+
+    mov rax, 1                  ; write syscall
+    mov rsi, rsp                ; Character
+    mov rdi, 1                  ; stdout
+    mov rdx, 1                  ; one byte
+    syscall
+
+end:
+
+    pop rdi
+    pop rdx
+    pop rsi
+
+    mov rsp, rbp                    ; epilogue
+    pop rbp
+
+    ret
