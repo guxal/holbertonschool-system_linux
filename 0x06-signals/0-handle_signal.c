1e3b21e7818755b784a4aaab5882351e2d5573d9
guxal <jonathancp93@gmail.com>
2021-02-19 12:52:50 +0000

    add comments and betty fix

--- 0x06-signals/0-handle_signal.c
+++ 0x06-signals/0-handle_signal.c
@@ -1,6 +1,10 @@
 #include "signals.h"
 
-
+/**
+ * signal_handler - Action to take in case signal
+ * @signum: Signal number
+ * Return: 0 on Success, or -1 on Error
+ */
 void signal_handler(int signum)
 {
 	char msg[] = "Gotcha! [0]\n";
@@ -9,10 +13,14 @@ void signal_handler(int signum)
 	write(1, msg, 12);
 }
 
-
+/**
+ * handle_signal - handles SIGINT signal
+ * Return: 0 on success, or -1 on error
+ */
 int handle_signal(void)
 {
 	if (signal(SIGINT, signal_handler) == SIG_ERR)
 		return (-1);
 	return (0);
 }
+
